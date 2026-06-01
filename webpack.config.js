d862a7308342a3425c9c763f0b4e6a14b95c19df
Zipakos <pocopiko_1990@hotmail.com>
2026-06-01 11:02:36 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,11 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   
-  // Provide the correct polyfill fallback using the package we just installed
+  // Maps both missing Node environments to browser equivalents
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    crypto: require.resolve('crypto-browserify')
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify')
   };
   
   return config;
