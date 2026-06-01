9ad4756b3faeaa5bd61d2880f26ed7c3030109d7
Zipakos <pocopiko_1990@hotmail.com>
2026-06-01 10:35:57 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+  // Custom build adjustments can be injected here if needed
+  return config;
+};
